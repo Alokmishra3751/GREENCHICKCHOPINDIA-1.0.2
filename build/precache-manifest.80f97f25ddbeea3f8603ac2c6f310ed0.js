5a949e0f364bb82c27d3f52588b6fbc8c3cc61da
Alok mishra <37037009+Alokmishra3751@users.noreply.github.com>
2021-10-21 01:48:02 +0530

    Add files via upload

---
+++ build/precache-manifest.80f97f25ddbeea3f8603ac2c6f310ed0.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b2e27791d4108d464a74261878d55a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70fc7d72182e0a64d53c",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "ac0eb9039d1d12f4ac58",
+    "url": "/static/css/main.df170736.chunk.css"
+  },
+  {
+    "revision": "70fc7d72182e0a64d53c",
+    "url": "/static/js/2.c74b8af8.chunk.js"
+  },
+  {
+    "revision": "2edd55c32008c186fb1d9cbe2ec10e03",
+    "url": "/static/js/2.c74b8af8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac0eb9039d1d12f4ac58",
+    "url": "/static/js/main.5b7bcf11.chunk.js"
+  },
+  {
+    "revision": "cbfff7819c54028c37e0",
+    "url": "/static/js/runtime-main.5a0c61e1.js"
+  },
+  {
+    "revision": "7fffd2d59b1d1d7c1933cd2343e8fc02",
+    "url": "/static/media/Spinner.7fffd2d5.gif"
+  },
+  {
+    "revision": "5f362ce3ed53ab04c99ac3b40fab712b",
+    "url": "/static/media/both-food.5f362ce3.jpg"
+  },
+  {
+    "revision": "7c5ef8baf8b930dc2d27e6006f259938",
+    "url": "/static/media/facebook.7c5ef8ba.png"
+  },
+  {
+    "revision": "6fdce6c14210c204695f0fd2c888762c",
+    "url": "/static/media/instagram.6fdce6c1.png"
+  },
+  {
+    "revision": "a067a5243a8168ed7d2ebcb788c030d9",
+    "url": "/static/media/landing1.a067a524.jpg"
+  },
+  {
+    "revision": "494ebef7ec8ed46820ecb9e50865c2c1",
+    "url": "/static/media/landing2.494ebef7.jpg"
+  },
+  {
+    "revision": "8ad78b5b8ad4a9c60478e52adf8f7da2",
+    "url": "/static/media/landing3.8ad78b5b.jpg"
+  },
+  {
+    "revision": "d240ec8c291644e79af4b38cdafe9f98",
+    "url": "/static/media/landing4.d240ec8c.jpg"
+  },
+  {
+    "revision": "594a81bdfeaf912b6d3231ae9e6991fe",
+    "url": "/static/media/non-veg.594a81bd.jpg"
+  },
+  {
+    "revision": "7940f4a0b6a24937b734061b6e2c50c5",
+    "url": "/static/media/primaryAppIcon.7940f4a0.png"
+  },
+  {
+    "revision": "02410493a843040d1569acacb093a078",
+    "url": "/static/media/twitter.02410493.png"
+  },
+  {
+    "revision": "968238995b6a557109c6299ad11254e2",
+    "url": "/static/media/veg.96823899.jpg"
+  }
+]);
